1cbf6d37540e2159bec5c359c466c5558dc44671
JINFENGHU6CB3\jinfenghu <kevin.hu@purple.telstra.com>
2023-08-26 20:56:42 +1000

    Add Product variant in the detail page

--- BlazorEcommerce/BlazorEcommerce/Server/Services/Products/ProductService.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Services/Products/ProductService.cs
@@ -13,7 +13,10 @@
         {
             var response = new ServiceResponse<Product>();
 
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                                        .Include(p => p.Variants)
+                                        .ThenInclude(v => v.ProductType)
+                                        .FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null) 
             {
                 response.Success = false;
@@ -30,7 +33,7 @@
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
 
             return response;
@@ -41,6 +44,7 @@
             var response = new ServiceResponse<List<Product>>()
             {
                 Data = await _context.Products
+                                     .Include(p => p.Variants)
                                      .Where(p => p.Category != null && p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
                                      .ToListAsync()
             };
---
+++ BlazorEcommerce/BlazorEcommerce/Shared/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> values)
+        {
+            if (values == null) return true;
+
+            return !values.Any();
+        }
+    }
+}
